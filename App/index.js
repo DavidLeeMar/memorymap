738371601abb63ab3679f9ee49be83b80ba4c0fb
David Mar <dave.mar@gmail.com>
2020-09-15 16:08:28 -0700

    save

--- App/index.js
+++ App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import Navigation from './config/navigation';
+import Navigation from './config/navigation';
 import App from './screens/map.js';
 
 export default () => <App />;
